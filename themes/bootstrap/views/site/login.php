a53a7f173dbc1e8c7aeb5424a259bc2c8990980d
marcelo epuyao <marceloepuyao@github.com>
2014-06-12 22:46:06 -0400

    instrucciones

--- themes/bootstrap/views/site/login.php
+++ themes/bootstrap/views/site/login.php
@@ -29,7 +29,7 @@ $this->breadcrumbs=array(
 	<?php echo $form->textFieldRow($model,'username'); ?>
 
 	<?php echo $form->passwordFieldRow($model,'password',array(
-        'hint'=>'Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>',
+        
     )); ?>
 
 	<?php echo $form->checkBoxRow($model,'rememberMe'); ?>
